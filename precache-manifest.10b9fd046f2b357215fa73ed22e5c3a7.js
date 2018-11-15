3e262ee85c86e361943017159e226c278622e6c5
sondrehusev@gmail.com <sondrehusev@gmail.com>
2018-11-16 00:40:41 +0100

    Updates

---
+++ precache-manifest.10b9fd046f2b357215fa73ed22e5c3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "611f3c34236546ef1cb0",
+    "url": "/CyberpunkRPG/static/js/runtime~main.611f3c34.js"
+  },
+  {
+    "revision": "c4903b2978099316ca69",
+    "url": "/CyberpunkRPG/static/js/main.c4903b29.chunk.js"
+  },
+  {
+    "revision": "d5de9d4cf59f873ca269",
+    "url": "/CyberpunkRPG/static/js/1.d5de9d4c.chunk.js"
+  },
+  {
+    "revision": "c4903b2978099316ca69",
+    "url": "/CyberpunkRPG/static/css/main.8b202f24.chunk.css"
+  },
+  {
+    "revision": "67e966e747f941a98f7b131a59b2084d",
+    "url": "/CyberpunkRPG/index.html"
+  }
+];
