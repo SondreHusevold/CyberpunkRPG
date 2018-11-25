124d1a3b040fcf192ac1bbd07ac073df716e5610
sondrehusev@gmail.com <sondrehusev@gmail.com>
2018-11-25 23:19:33 +0100

    Added vampire epilogue written for WerewolfRPG

--- src/FileService.ts
+++ src/FileService.ts
@@ -2,10 +2,23 @@ export class FileService {
 
 	public static async getFiles() {
 		 const files = fetch('MainframeStructure.json');
-		 return files.then((s) => s.json().then((data: IStructure) => {
+		 return files.then((s) => s.json().then(async (data: IStructure) => {
+				// Apply the epilogue to the folder structure since it is in a seperate file.
+				
+				 const epilogue = await this.getVampireEpilogue();
+				 /* tslint:disable:no-string-literal */
+				 data.folders["home"].folders["mainframe"].folders["Vampire"].folders["Epilogue_RECOVERED.dat"].result = epilogue.epilogue;
+
 				return data;
 			})
 		);
+	}
+
+	public static async getVampireEpilogue() {
+		const epilogue = fetch('VampireEpilogue.json');
+		return epilogue.then((s) => s.json().then((data: { epilogue: string }) => {
+			return data;
+		}));
 	}
 
 }
