e15415b1f663358d902f228da544777ff65af08b
Sondre Husevold <sondrehusev@gmail.com>
2019-12-07 16:50:30 +0100

    Updates

---
+++ precache-manifest.73d37330d4af90a495d4aa782af9ca88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9da4efd2108007857ac5fec0428ba9",
+    "url": "/CyberpunkRPG/index.html"
+  },
+  {
+    "revision": "bee95fb5a871ee3bdb44",
+    "url": "/CyberpunkRPG/static/css/2.06aa897a.chunk.css"
+  },
+  {
+    "revision": "e87bbf7a04c9db517203",
+    "url": "/CyberpunkRPG/static/css/main.c60ee4cc.chunk.css"
+  },
+  {
+    "revision": "bee95fb5a871ee3bdb44",
+    "url": "/CyberpunkRPG/static/js/2.efa6f7a4.chunk.js"
+  },
+  {
+    "revision": "e87bbf7a04c9db517203",
+    "url": "/CyberpunkRPG/static/js/main.a616f7a3.chunk.js"
+  },
+  {
+    "revision": "7ed10bad92387e6d8812",
+    "url": "/CyberpunkRPG/static/js/runtime~main.7e13717d.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/CyberpunkRPG/static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/CyberpunkRPG/static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/CyberpunkRPG/static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/CyberpunkRPG/static/media/MaterialIcons-Regular.e79bfd88.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CyberpunkRPG/precache-manifest.8711f2577d7c1fe8bb25ae38f6a125b3.js"
+  "/CyberpunkRPG/precache-manifest.73d37330d4af90a495d4aa782af9ca88.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/CyberpunkRPG/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CyberpunkRPG/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
